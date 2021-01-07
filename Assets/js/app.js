178d069bbeaa0a8e78508a2c85041ca191b9f4b2
HomS <obr_dev@protonmail.com>
2021-01-07 11:09:14 -0600

    Added Flip, Zoom Animations

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,48 +1,37 @@
-const boxes= document.querySelectorAll('.box');
-const boxy=document.querySelector('.box');
+const boxes = document.querySelectorAll(".box");
+const boxy = document.querySelector(".box");
 
-boxy.classList.add("ani")
-boxy.classList.remove()
-boxy.style.transform="translate3d(0,0,0)"
+boxy.classList.add("ani");
+boxy.classList.remove();
 
-
-const options={
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.7
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.7,
 };
 
-function scrollELement(element,animation,options){
-    
-    const internalCallback = function( entries) {
-        let observedElement = entries[0];
-        
-        if(observedElement.isIntersecting){
-            element.classList.add("ani-scroll");
-        }else{
-            element.classList.remove("ani-scroll");
-        }
-    };
+function scrollELement(element, options) {
+  const internalCallback = function (entries) {
+    let observedElement = entries[0];
 
-    const observer = new IntersectionObserver( internalCallback, options );
-    observer.observe(element);
+    // if (observedElement.isIntersecting) {
+    //   element.classList.add("ani-scroll");
+    // } else {
+    //   element.classList.remove("ani-scroll");
+    // }
+
+    observedElement.isIntersecting
+      ? element.classList.add("min-ani-scroll")
+      : element.classList.remove("min-ani-scroll");
+  };
+
+  const observer = new IntersectionObserver(internalCallback, options);
+  observer.observe(element);
 }
 
-boxes.forEach(box=>{
-    scrollELement(box,"",options);
-})
+boxes.forEach((box) => {
+  scrollELement(box, options);
+});
 
-const title= document.getElementById("title");
-scrollELement(title,"",options);
-
-
-
-
-
-
-
-
-
-
-
-
+const title = document.getElementById("title");
+scrollELement(title, options);
